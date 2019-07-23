c41380f787e1c340b6ee4004b10eb5e1c1369831
Vijay Patidar <vramotiya@gmail.com>
2019-07-23 05:56:32 +0000

    trst

--- app/src/main/java/com/svgi/lectureschedule/MainActivity.java
+++ app/src/main/java/com/svgi/lectureschedule/MainActivity.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import javax.annotation.Nullable;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     private Map<String,String> SUBJECTS, FACULTY, ROOM_NUM;
